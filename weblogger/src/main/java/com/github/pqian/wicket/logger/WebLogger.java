b8c6047ebf001687e4367c0b0f3426d67f0a3399
pqian <pqian@mint14>
2013-06-02 00:03:49 +0200

    first impl

--- weblogger/src/main/java/com/github/pqian/wicket/logger/WebLogger.java
+++ weblogger/src/main/java/com/github/pqian/wicket/logger/WebLogger.java
@@ -1,5 +1,8 @@
 package com.github.pqian.wicket.logger;
 
+import java.io.IOException;
+import java.io.PipedReader;
+import java.io.PipedWriter;
 import java.io.Reader;
 import java.io.Writer;
 
@@ -7,15 +10,30 @@ import org.apache.wicket.protocol.http.WebApplication;
 
 public class WebLogger {
 
+	private static class ReaderWriterHolder {
+		private static Reader reader;
+		private static Writer writer;
+		
+		static {
+			reader = new PipedReader();
+			try {
+				writer = new PipedWriter((PipedReader)reader);
+			} catch (IOException e) {
+				new RuntimeException("Cannot instantiate a PipedWriter with the given PipedReader", e);
+			}
+		}
+		
+	}
+	
 	public static void configure(WebApplication webApplication) {
 		webApplication.mountPage("/logger", WebLoggerPage.class);
 	}
 	
 	public static Writer getWriter() {
-		return null;
+		return ReaderWriterHolder.writer;
 	}
 	
 	public static Reader getReader() {
-		return null;
+		return ReaderWriterHolder.reader;
 	}
 }
--- weblogger/src/main/java/com/github/pqian/wicket/logger/WebLoggerPage.java
+++ weblogger/src/main/java/com/github/pqian/wicket/logger/WebLoggerPage.java
@@ -1,24 +1,67 @@
 package com.github.pqian.wicket.logger;
 
+import java.io.IOException;
+import java.io.LineNumberReader;
+
+import org.apache.wicket.Component;
+import org.apache.wicket.ajax.AbstractAjaxTimerBehavior;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.attributes.AjaxCallListener;
+import org.apache.wicket.ajax.attributes.AjaxRequestAttributes;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.model.AbstractReadOnlyModel;
+import org.apache.wicket.util.time.Duration;
 
 public class WebLoggerPage extends WebPage {
 
 	private static final long serialVersionUID = 1L;
 	
 	public WebLoggerPage() {
-		add(new Label("loggerView", new AbstractReadOnlyModel<String>() {
+		final LineNumberReader reader = new LineNumberReader(WebLogger.getReader());
+		final Label buffer = new Label("buffer", new AbstractReadOnlyModel<String>() {
 
 			private static final long serialVersionUID = 1L;
 
 			@Override
 			public String getObject() {
-				// WebLogger.getReader();
-				return "";
+				StringBuilder lines = new StringBuilder();
+				String line = null;
+				int count = 0;
+				try {
+					while(count++<5 && (line = reader.readLine())!=null) {
+						lines.append(line).append('\n');
+					}
+				} catch (IOException e) {
+					throw new RuntimeException("Read logger failed", e);
+				}
+				return lines.toString();
 			}
-		}));
+		});
+		add(buffer.setOutputMarkupId(true));
+		buffer.add(new AbstractAjaxTimerBehavior(Duration.seconds(2)) {
+			
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			protected void onTimer(AjaxRequestTarget target) {
+				target.add(buffer);
+			}
+			
+			@Override
+			protected void updateAjaxAttributes(AjaxRequestAttributes attributes) {
+				super.updateAjaxAttributes(attributes);
+				attributes.getAjaxCallListeners().add(new AjaxCallListener() {
+
+					private static final long serialVersionUID = 1L;
+				
+					@Override
+					public CharSequence getSuccessHandler(Component component) {
+						return "def pre = $(<pre></pre>).html($(#buffer).html());$(body).append(pre);" + super.getSuccessHandler(component);
+					}
+				});
+			}
+		});
 	}
 
 }
